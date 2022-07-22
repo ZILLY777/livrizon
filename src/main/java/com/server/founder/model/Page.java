75f0fb6f2da86449ef6edb95dc6a43e214f314fe
Daniil <daniil@poliven.ru>
2022-07-22 15:36:40 +0300

    i loh

--- src/main/java/com/server/founder/model/Page.java
+++ src/main/java/com/server/founder/model/Page.java
@@ -13,13 +13,11 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 @JsonInclude(NON_NULL)
 public class Page {
     UserInformation user;
-    List<FileView> preview;
     List<Post> posts;
     public Page() {
     }
-    public Page(UserInformation information, List<FileView> preview, List<Post> posts) {
+    public Page(UserInformation information,  List<Post> posts) {
         this.user = information;
-        this.preview = preview;
         if(posts.size()>0) this.posts = posts;
     }
 }
